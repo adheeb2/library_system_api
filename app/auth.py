b85a2ce8049a2a7cd9d73d7be73013ebe3e712d7
adheeb2 <adheebanvar2@gmail.com>
2025-01-24 12:48:45 +0530

    added librarian user function in auth file

--- app/auth.py
+++ app/auth.py
@@ -52,5 +52,12 @@ def get_current_user(token : str = Depends(oauth2_scheme), db: Session = Depends
 def get_admin_user(current_user: User = Depends(get_current_user)): 
     if current_user.role != "Admin":
         raise HTTPException(status_code=403, detail="Not enough permissions")
-    return current_user
+    return 
+
+def get_librarian_user(current_user : User = Depends(get_current_user)):
+    if current_user.role not in ["Librarian", "Admin"]:
+        raise HTTPException(
+            status_code=403,
+            detail="Not enough permissions"
+        )
 
